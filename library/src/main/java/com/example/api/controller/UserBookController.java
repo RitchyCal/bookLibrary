4189bbdf08fc4817ea75c8120f53c7c6f4068570
richy163 <ritchycal@gmail.com>
2024-06-19 17:20:05 -0400

    added front end react type script

--- library/src/main/java/com/example/api/controller/UserBookController.java
+++ library/src/main/java/com/example/api/controller/UserBookController.java
@@ -3,7 +3,6 @@ package com.example.api.controller;
 import com.example.api.model.UserBooks;
 import com.example.service.UserBookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
